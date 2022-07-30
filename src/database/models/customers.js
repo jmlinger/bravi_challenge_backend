a4e418856223a8f2f463af7529d92a77ba2f741a
jmlinger <jmlinger10@gmail.com>
2022-07-30 13:46:06 -0300

    fix: insert customers model.

---
+++ src/database/models/customers.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const customers = sequelize.define('customers', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    address: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    whatsapp: DataTypes.STRING,
+    socialMediaLink: DataTypes.STRING,
+  }, 
+  { timestamps: false, tableName: 'customers', underscored: true });
+
+  return customers;
+};
