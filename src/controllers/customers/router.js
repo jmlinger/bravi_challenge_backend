375de6dcaf58e7b2d1e4b60a4594790b745b533c
jmlinger <jmlinger10@gmail.com>
2022-07-30 13:44:21 -0300

    feat: list custumers route.

---
+++ src/controllers/customers/router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const rescue = require('express-rescue');
+
+const list = require('./list');
+const create = require('./create');
+const update = require('./update');
+const remove = require('./remove');
+
+const router = express.Router({ mergeParams: true });
+
+router.get('/', rescue(list));
+router.post('/', rescue(create));
+router.put('/:id', rescue(update));
+router.delete('/:id', rescue(remove));
+
+module.exports = router;
