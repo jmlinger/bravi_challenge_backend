525fd5704e0651df5920b74fa1fa14c3fe868b7f
jmlinger <jmlinger10@gmail.com>
2022-08-03 23:28:09 -0300

    feat: add pattern validation that accept only number on phone and whatapp values.

--- src/utils/validations/customers.js
+++ src/utils/validations/customers.js
@@ -2,10 +2,20 @@ const Joi = require('joi');
 
 const customerSchema = Joi.object({
   name: Joi.string().min(3).required(),
-  email: Joi.string().email().required(),
+  email: Joi.string()
+    .email()
+    .required(),
   address: Joi.string().min(10).required(),
-  phone: Joi.string().allow('').required().length(11),
-  whatsapp: Joi.string().allow('').required().length(12),
+  phone: Joi.string()
+    .allow('')
+    .required()
+    .length(11)
+    .pattern(/^[0-9]+$/),
+  whatsapp: Joi.string()
+    .allow('')
+    .required()
+    .length(12)
+    .pattern(/^[0-9]+$/),
   socialMediaLink: Joi.string().allow('').required().uri(),
 });
 
